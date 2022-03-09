63eec10127e814d30dcf0494a8210101c0c741c6
Cleiton Candiotto <cleitoncandiotto@gmail.com>
2022-03-09 14:59:20 -0300

    Relacionando Modelos com Bando de Dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -6,7 +6,7 @@ from apps.funcionarios.models import Funcionario
 class Documento(models.Model):
     descricao = models.CharField('Descrição', max_length=70, help_text='Descrição do Documento')
     # um documento pode pertencer para um empregado
-    pertence = models.ForeignKey(Funcionario, on_delete=models.DO_NOTHING) 
+    pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT) 
 
 
     def __str__(self):
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,14 +1,16 @@
+from statistics import mode
 from django.db import models
 from django.contrib.auth.models import User
 from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 
 
 class Funcionario(models.Model):
     nome = models.CharField('Funcionario', max_length=100, help_text='Nome Funcionario')
-    user = models.ForeignKey(User, models.PROTECT)
+    user = models.OneToOneField(User, models.PROTECT)
     departamentos = models.ManyToManyField(Departamento) # um funcionario pode estar em vários departamentos
-
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
     
     def __str__(self) :
         return self.nome
