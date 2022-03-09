5067ede58a69d473db15cf001592752eb8e43ea4
Cleiton Candiotto <cleitoncandiotto@gmail.com>
2022-03-09 14:00:41 -0300

    Relacionamentos de Banco de Dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,9 +1,12 @@
 from statistics import mode
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 
 class Documento(models.Model):
     descricao = models.CharField('Descrição', max_length=70, help_text='Descrição do Documento')
+    # um documento pode pertencer para um empregado
+    pertence = models.ForeignKey(Funcionario, on_delete=models.DO_NOTHING) 
 
 
     def __str__(self):
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
 
 
 
 class Funcionario(models.Model):
     nome = models.CharField('Funcionario', max_length=100, help_text='Nome Funcionario')
+    user = models.ForeignKey(User, models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento) # um funcionario pode estar em vários departamentos
 
     
     def __str__(self) :
