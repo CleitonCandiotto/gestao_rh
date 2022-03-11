720136c79c202e34187fa8a47cea0689d2c6a272
Cleiton Candiotto <cleitoncandiotto@gmail.com>
2022-03-11 15:45:51 -0300

    Crud Funcionarios

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,10 +1,12 @@
+from unicodedata import name
 from django.urls import path
-from .views import FuncionarioUpdateView, FuncionariosListView, FuncionarioDeleteView
+from .views import FuncionarioUpdateView, FuncionariosListView, FuncionarioDeleteView, FuncionarioCreateView
 
 
 urlpatterns = [
     path('', FuncionariosListView.as_view(), name='list_funcionarios'),
     path('edit/<int:pk>/', FuncionarioUpdateView.as_view(), name='edit_funcionario'),
-    path('delete/<int:pk>/', FuncionarioDeleteView.as_view(), name='delete_funcionario')
+    path('delete/<int:pk>/', FuncionarioDeleteView.as_view(), name='delete_funcionario'),
+    path('create/', FuncionarioCreateView.as_view(), name='create_funcionario')
     
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,7 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import Funcionario
+from django.contrib.auth.models import User
+
 
 
 class FuncionariosListView(ListView):
@@ -25,3 +27,18 @@ class FuncionarioDeleteView(DeleteView):
     fields = ['nome', 'departamentos']
     template_name = 'funcionario_delete.html'
     success_url = reverse_lazy('list_funcionarios')
+
+
+class FuncionarioCreateView(CreateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+    template_name = 'funcionario_create.html'
+
+
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        username = funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[1]
+        funcionario.empresa = self.request.user.funcionario.empresa
+        funcionario.user = User.objects.create(username=username)
+        funcionario.save()
+        return super(FuncionarioCreateView, self).form_valid(form)
