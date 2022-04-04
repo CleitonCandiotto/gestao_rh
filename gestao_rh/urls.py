13e8249a4ca222d42380c9500103dd66e9b732f6
CleitonCandiotto <cleitoncandiotto@gmail.com>
2022-04-04 16:05:21 -0300

    Conserto Ajax

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
+
 
 from rest_framework import routers
 from core import views
@@ -22,7 +22,7 @@ urlpatterns = [
 ]
 
 
-urlpatterns += i18n_patterns (
+urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('funcionarios/', include('apps.funcionarios.urls'), name='funcionarios'),
@@ -34,4 +34,4 @@ urlpatterns += i18n_patterns (
 
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
- ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
